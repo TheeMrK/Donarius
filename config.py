fc1c65c24862c1c0b9d88d9b54a6a7b933e8a534
TheeMrK <chriskleinschmidt@yahoo.com>
2025-06-19 14:00:15 -0700

    Update config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+PREFIX = "!"
+DEFAULT_EMOJI = "🎉"
+EVENTS_CHANNEL_ID = 
+LOGS_CHANNEL_ID = 
+ALLOWED_ROLE_IDS = [
+    # your_role_ids_here,separated_by_commas
+    # Example: 123456789012345678,234567890123456789
+]
